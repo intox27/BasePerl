beafc588af2fc5ca2b4f90e855f1751bcc0e563d
intox27 <bastien.cuissard@free.fr>
2015-05-23 16:49:07 +0200

    ajout boucle while et messages

--- irand.pl
+++ irand.pl
@@ -3,3 +3,23 @@ use warnings;
 
 my $nb = int (rand 101);
 my $essai = -1;
+
+print "Devinez le nombre mystere.\n";
+
+while ($essai != $nb)
+{
+    print '> ';
+    $essai = <>;
+
+    if ($essai < $nb)
+    {
+        print "C'est plus.\n";
+    }
+    elsif ($essai > $nb)
+    {
+        print "C'est moins.\n";
+    }
+}
+
+print "Gagné";
+<>;

faaa68060b7e72bb4c51ff022d1504bd1efaee8f
intox27 <bastien.cuissard@free.fr>
2015-05-23 16:44:56 +0200

    ajout variables, messages et branchement conditionnel

---
+++ idevine.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+print "Choisissez un animal parmi les suivants :\n"; 
+print "Chat, Mouette, Lama, Chouette\n";
+print '-' x 30 . "\n";
+
+print 'Est-ce que c\'est un oiseau [o/n] ? ';
+my $choice = <>;
+chomp $choice;
+
+if (lc($choice) eq 'o')
+{
+    print 'Est-ce que c\'est un oiseau nocturne [o/n] ? ';
+    $choice = <>;
+    chomp $choice;
+    if (lc($choice) eq 'o')
+    {
+        print "C'est une chouette !\n";   
+    }
+    else
+    {
+        print "C'est une mouette !\n";
+    }
+}
+else
+{
+    print 'Est-ce que cet animal crache [o/n] ? ';
+    $choice = <>;
+    chomp $choice;
+    if (lc($choice) eq 'o')
+    {
+        print "C'est un lama !\n";
+    }
+    else
+    {
+        print "C'est un chat !\n";
+    }
+}
